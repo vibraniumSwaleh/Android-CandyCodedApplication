8b3361bf686de7234df58177d66b496b2e1a6d46
vibraniumSwaleh <hus.swaleh@yahoo.com>
2019-10-27 10:43:38 +0300

    createPhoneIntent(View view) created

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.net.URI;
+
 public class InfoActivity extends AppCompatActivity {
 
     @Override
@@ -41,4 +43,9 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent intentDial = new Intent(Intent.ACTION_DIAL);
+        intentDial.setData(Uri.parse("tel:0123456789"));
+        startActivity(intentDial);
+    }
 }
